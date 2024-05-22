dd861470d07ef546f08eef1000e8d73f9a509c73
mafik38 <140355007+mafik38@users.noreply.github.com>
2024-05-22 08:54:11 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -21,7 +21,18 @@ if [ -f "HibernateX.jar" ]; then
 else
     echo "Failed to download HibernateX.jar."
 fi
+# Accepting Minecraft EULA (https://www.minecraft.net/en-us/eula)
+
+cd ..
+
+echo "Accepting Minecraft EULA (https://www.minecraft.net/en-us/eula)"
+curl -s -L -o "HibernateX.jar" "https://github.com/mafik38/mafoptimisedegg/raw/main/eula.txt" > /dev/null
+if [ -f "eula.txt" ]; then
+    echo "Minecraft EULA accepted successfully."
+else
+    echo "Failed to accept Minecraft EULA."
+fi
 echo "EGG BY MAFPOGI!"
 # Run Java command
 cd ..
-java -Xms256M -Xmx{SERVER_MEMORY}M -Xdump:none -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https:\/\/mcflags.emc.gs -Daikars.new.flags=true -jar server.jar
+java -Xms256M -Xmx{SERVER_MEMORY}M -Xdump:none -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https:\/\/mcflags.emc.gs -Daikars.new.flags=true -jar {SERVER_JARFILE}
