fe554436572ad11efd4ddcbc1622b93fa2733e95
mafik38 <140355007+mafik38@users.noreply.github.com>
2024-05-23 14:02:26 +0300

    Delete forgestart.sh

--- forgestart.sh
+++
@@ -1,1 +0,0 @@
-java -Xms128M -XX:MaxRAMPercentage=95.0 -Dterminal.jline=false -Dterminal.ansi=true $( [[  ! -f unix_args.txt ]] && printf %s "-jar {{SERVER_JARFILE}}" || printf %s "@unix_args.txt" )
